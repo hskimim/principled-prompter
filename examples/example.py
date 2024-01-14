9092df63dad69459f62bee3a033acf81f7a2cf90
hskimim <hskimim8855@gmail.com>
2024-01-15 03:13:36 +0900

    update example code

--- examples/example.py
+++ examples/example.py
@@ -7,45 +7,37 @@ from principled_prompter import agent  # noqa: E402
 
 if __name__ == "__main__":
     model = agent.PromptCalibrator(num_agent=3)
-    result = model.calibrate(question="List the main causes of the French Revolution.")
+    result = model.calibrate(
+        question="Craft a beginner's introduction to the concept of gravity. Aim it at students in the early stages of elementary school.",
+    )
     pprint(result.model_dump())
 
-# {'calibrated_question': 'Please provide the additional details for the request '
-#                         'in a chain-of-thought format as follows:\n'
-#                         'Example 1: "Identify the main causes of the French '
-#                         'Revolution. The main causes of the French Revolution '
-#                         'include economic hardships, political instability, '
-#                         'social inequality, and enlightenment ideas. Economic '
-#                         'hardships refer to the financial struggles faced by '
-#                         'the common people, which were exacerbated by high '
-#                         'taxation and poor harvests. Political instability '
-#                         'pertains to the weak leadership and lack of '
-#                         'representation, leading to a sense of injustice and '
-#                         'unrest. Social inequality involves the stark '
-#                         'divisions between the nobility, clergy, and '
-#                         'commoners, leading to deep societal grievances. '
-#                         'Enlightenment ideas, rooted in concepts of liberty, '
-#                         'equality, and fraternity, fostered a questioning of '
-#                         'traditional authority and norms, fueling '
-#                         'revolutionary sentiment."\n'
-#                         'Main Question: "Elaborate on \'economic hardships,\' '
-#                         "'political instability,' 'social inequality,' and "
-#                         "'enlightenment ideas' as contributing factors to the "
-#                         'French Revolution."',
-#  'followed_principles': ['Implement example-driven prompting (Use few-shot '
-#                          'prompting).',
-#                          'Combine Chain-of-thought (CoT) with few-Shot '
-#                          'prompts.',
-#                          'Use output primers, which involve concluding your '
-#                          'prompt with the beginning of the desired output. '
-#                          'Utilize output\n'
-#                          'primers by ending your prompt with the start of the '
-#                          'anticipated response.\n',
-#                          'When formatting your prompt, start with '
-#                          '‘###Instruction###’, followed by either '
-#                          '‘###Example###’\n'
-#                          'or ‘###Question###’ if relevant. Subsequently, '
-#                          'present your content. Use one or more\n'
-#                          'line breaks to separate instructions, examples, '
-#                          'questions, context, and input data.'],
-#  'question': 'List the main causes of the French Revolution.'}
+{
+    "calibrated_question": "Explain to me like I'm 9 years old: What is gravity "
+    "and how does it affect things on Earth? You MUST "
+    "provide an example of gravity in action and explain "
+    "how it's connected to the Earth and other objects in "
+    "space. You will be penalized if you don't use simple "
+    "language.",
+    "followed_principles": [
+        "Integrate the intended audience in the prompt, e.g., "
+        "the audience is an expert in the field.",
+        "Employ affirmative directives such as ‘do,’ while "
+        "steering clear of negative language like ‘don’t’.",
+        "When you need clarity or a deeper understanding of a "
+        "topic, idea, or any piece of information, utilize "
+        "the\n"
+        "following prompts:\n"
+        "o Explain [insert specific topic] in simple terms.\n"
+        "o Explain to me like I’m 11 years old.\n"
+        "o Explain to me as if I’m a beginner in [field].\n"
+        "o Write the [essay/text/paragraph] using simple "
+        "English like you’re explaining something to a "
+        "5-year-old.\n",
+        "Incorporate the following phrases: “Your task is” " "and “You MUST”.",
+        "Incorporate the following phrases: “You will be " "penalized”.",
+        "Use leading words like writing “think step by " "step”.",
+    ],
+    "question": "Craft a beginner's introduction to the concept of gravity. Aim "
+    "it at students in the early stages of elementary school.",
+}
